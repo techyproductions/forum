75afd521bdad01e20497f9a52ba2102a730f0eec
mgaudiophile <mgaudiofile@gmail.com>
2020-05-31 13:39:22 -0600

    added navbar

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import './App.css';
 
 import HomeBackground from './Components/HomeBackground/HomeBackground';
+import NavBar from './Components/NavBar/NavBar';
 
 function App() {
   return (
     <div className="App">
+        <NavBar></NavBar>
       <HomeBackground></HomeBackground>
     </div>
   );
---
+++ src/Components/NavBar/NavBar.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import classes from './NavBar.module.css';
+import { Link, animateScroll as scroll } from 'react-scroll';
+
+class NavBar extends Component {
+    render() {
+        return (
+            <nav className={classes.Nav}>
+                <div className={classes.NavRight}>
+                    <Link className={classes.NavLink}>Home</Link>
+                    <span className={classes.Dot}></span>
+                    <Link className={classes.NavLink}>Forum</Link>
+                    <span className={classes.Dot}></span>
+                </div>
+            </nav>
+        );
+    }
+}
+
+export default NavBar;
