1f00233ee8d950c3e7bf8e74c4c204fa1f9fce31
mgaudiophile <mgaudiofile@gmail.com>
2020-05-31 19:35:40 -0600

    upgraded navbar to bootstrap navbar

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,18 +1,28 @@
 import React, { Component } from 'react';
-import classes from './NavBar.module.css';
-import { Link, /*animateScroll as scroll*/ } from 'react-scroll';
+import Navbar from 'react-bootstrap/Navbar';
+import FormControl from 'react-bootstrap/FormControl';
+import Nav from 'react-bootstrap/Nav';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+
+//import classes from './NavBar.module.css';
+//import { Link, /*animateScroll as scroll*/ } from 'react-scroll';
 
 class NavBar extends Component {
     render() {
         return (
-            <nav className={classes.Nav}>
-                <div className={classes.NavRight}>
-                    <Link className={classes.NavLink}>Home</Link>
-                    <span className={classes.Dot}></span>
-                    <Link className={classes.NavLink}>Forum</Link>
-                    <span className={classes.Dot}></span>
-                </div>
-            </nav>
+            <Navbar bg="light" variant="light">
+                <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+                <Nav className="mr-auto">
+                    <Nav.Link href="#home">Home</Nav.Link>
+                    <Nav.Link href="#features">Features</Nav.Link>
+                    <Nav.Link href="#pricing">Pricing</Nav.Link>
+                </Nav>
+                <Form inline>
+                    <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+                    <Button variant="outline-primary">Search</Button>
+                </Form>
+            </Navbar>
         );
     }
 }
